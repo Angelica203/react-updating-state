5cfa51e5affdc6b90c568f9ed7abd1e4ad4a2274
github username <github email address>
2021-08-14 20:02:04 -0400

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,33 @@
-// Code ClickityClick Component Here
+import React from 'react'
+class ClickityClick extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            hasBeenClicked: false
+        };
+    }
+    // handleClick = () => {
+    //     const newCount = this.state.count + 1
+    //     this.setState({
+    //         count: newCount
+    //     })
+    // };
+
+
+handleClick = () => {
+    this.setState({
+      hasBeenClicked: true
+    })
+  }
+    render(){
+        return (
+            <div>
+                <p> I have {this.state.hasBeenClicked ? null : 'not'} been clicked!</p>
+                <button onClick={this.handleClick}>Click me!</button>
+            </div>
+        )
+    }
+}
+
+export default ClickityClick;
+// export default ButtonCounter 
---
+++ src/components/LightSwitch.js
@@ -0,0 +1,25 @@
+// import React from "react";
+// class LightSwitch extends React.Component {
+//     constructor (){
+//         super();
+
+//         this.state = {
+//             toggled: false 
+//         }
+//     }
+//     handleClick = () => {
+//         this.setState(previousState => {
+//             return{
+//                 toggled: !previousState.toggled
+//             }
+//         })
+//     }
+
+//     redner(){
+//         return(
+//             <div>
+//                 <button onClick={this.handClick}>{this.state.toggled ? on :</button>
+//             </div>
+//         {/* ) */}
+//     }
+// }
